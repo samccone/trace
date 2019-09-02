36939d173f91b5a5100438b6bbfd5f942cce93b6
slu <slu@netflix.com>
2019-09-02 16:04:35 -0700

    initial data transforms for timeline

--- lib/format.d.ts
+++ lib/format.d.ts
@@ -1,12 +1,14 @@
 export interface TimelineEvent {
-    // Start time in miliseconds
-    start: number;
-    // End time in miliseconds
-    end: number;
-    // Text label to be associated with the timeline entry
-    label: string;
-    // Process / Thread that the events are associated with.
-    rowId: string;
+  // Start time in miliseconds
+  start: number;
+  // End time in miliseconds
+  end: number;
+  // Text label to be associated with the timeline entry
+  label: string;
+  // Process / Thread that the events are associated with.
+  rowId: string;
+
+  facet?: string;
 }
 
 export type TimelineEvents = TimelineEvent[];
--- lib/timeline.ts
+++ lib/timeline.ts
@@ -1,23 +1,72 @@
 import { Renderer } from "./renderers/renderer";
 import { TimelineEvents } from "./format";
+import {
+  scaleLinear,
+  scaleBand,
+} from "../node_modules/d3-scale/dist/d3-scale.js";
 
 export class Timeline {
-    constructor(public readonly renderer: Renderer, public readonly data: TimelineEvents) {
+  constructor(
+    public readonly renderer: Renderer,
+    public readonly data: TimelineEvents
+  ) {}
+
+  transformData(data: TimelineEvents) {
+    let xMin: number | undefined;
+    let xMax: number | undefined;
+    let facets: string[] = [];
+
+    data.forEach(d => {
+      if (!xMin || d.start < xMin) {
+        xMin = d.start;
+      }
+
+      if (!xMax || d.end > xMax) {
+        xMax = d.end;
+      }
+
+      if (
+        d.facet &&
+        typeof d.facet === "string" &&
+        facets.indexOf(d.facet) === -1
+      ) {
+        facets.push(d.facet);
+      }
+    });
+
+    const x = scaleLinear()
+      .domain([xMin || 0, xMax || 0])
+      .range([0, this.renderer.dimensions.width]);
+
+    if (facets.length === 0) {
+      facets.push("None");
     }
 
-    render() {
-       this.renderer.render([{
-           x: 100,
-           y: 100,
-           width: 50,
-           height: 20,
-           fill: 'black',
-           text: {
-               offsetX: 0,
-               offsetY: 0,
-               fill: 'white',
-               text: 'hello world'
-           }
-       }]); 
-    }
+    const y = scaleBand()
+      .domain(facets)
+      .range([0, this.renderer.dimensions.height])
+      .round(true);
+
+    const height = y.bandwidth();
+
+    return data.map(d => {
+      return {
+        x: x(d.start),
+        y: y(d.facet || "None") || 0,
+        width: x(d.end) - x(d.start),
+        height,
+        fill: "black",
+        text: {
+          offsetX: 0,
+          offsetY: 0,
+          fill: "white",
+          text: d.label,
+        },
+      };
+    });
+  }
+
+  render() {
+    this.renderer.render(this.transformData(this.data));
+  }
 }
